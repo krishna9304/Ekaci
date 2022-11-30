70fea7a153bc66e47568559449c55f4a7aca8cc7
Sourav Bera <sauravberaiit@gmail.com>
2022-11-30 14:35:50 +0530

    created data mask

--- machine_learning/natural_calamities_prediction/predict_model.py
+++ machine_learning/natural_calamities_prediction/predict_model.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+
+lr=LogisticRegression(max_iter=500)
+data=pd.read_csv("final_data.csv")
+data=np.array(data)
+data=data[:,1:]
+msk1 = np.random.rand(len(data)) < 0.7
+train = data[msk1]
+test = data[~msk1]
