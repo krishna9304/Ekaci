ccbe12cc5d7ed8aaa61bf67ee4e701e5c72169c8
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2022-11-30 16:40:06 +0530

    farmer, insurance, government registration frontend ui
    registration form built with context api, step by step process updation, etc.

---
+++ frontend/src/components/steps/Account.jsx
@@ -0,0 +1,189 @@
+import React, { useContext } from "react";
+import { StepperContext } from "../../contexts/StepperContext";
+
+const Account = () => {
+  const { partData, setPartData } = useContext(StepperContext);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPartData({ ...partData, [name]: value });
+  };
+  return (
+    <div className="flex flex-col">
+      {/* Farmer ID */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Farmer ID
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["farmer_id"] || ""}
+          name="farmer_id"
+          placeholder="FarmerID"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* First Name */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        First Name
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["first_name"] || ""}
+          name="first_name"
+          placeholder="First Name"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* Middle Name */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Middle Name
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["middle_name"] || ""}
+          name="middle_name"
+          placeholder="Middle Name"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* Last Name */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Last Name
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["last_name"] || ""}
+          name="last_name"
+          placeholder="Last Name"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* Post Office */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Post Office
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["post_office"] || ""}
+          name="post_office"
+          placeholder="Post Office"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* Police Station */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Police Station
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["police_station"] || ""}
+          name="police_station"
+          placeholder="Police Station"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* District */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        District
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["district"] || ""}
+          name="district"
+          placeholder="District"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* PinCode */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Pincode
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["pincode"] || ""}
+          name="pincode"
+          placeholder="Pin Code"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* State */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        State
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <input
+          onChange={handleChange}
+          value={partData["state"] || ""}
+          name="state"
+          placeholder="State"
+          type="text"
+          className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+        />
+      </div>
+
+      {/* Farmer Type */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Farmer type
+      </div>
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+        <select
+          id="farmer_type"
+          name="farmer_type"
+          value={partData["farmer_type"] || ""}
+          onChange={handleChange}
+          className="p-1 px-2  outline-none w-full text-gray-800"
+        >
+          <option value="user">Farmer Type</option>
+          <option value="small">Small</option>
+          <option value="marginal">Marginal</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      {/* Farmer Category */}
+      <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Farmer Category
+      </div>
+
+      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded ">
+        <select
+          id="farmer_category"
+          name="farmer_category"
+          value={partData["farmer_category"] || ""}
+          onChange={handleChange}
+          className="p-1 px-2  outline-none w-full text-gray-800"
+        >
+          <option value="user">Farmer Category</option>
+          <option value="farmer">Owner</option>
+          <option value="insurance">Tenant</option>
+          <option value="government">Shared cropping</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Account;
