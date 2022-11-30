a5978747eef044c476a7bc252bc09644b3ba98cc
Sourav Bera <sauravberaiit@gmail.com>
2022-11-30 16:05:38 +0530

    fask app for prediction of disaster LR

--- machine_learning/natural_calamities_prediction/app.py
+++ machine_learning/natural_calamities_prediction/app.py
@@ -7,7 +7,7 @@ import pickle
 
 
 app = Flask(__name__)
-
+#load the mdel fl sourav bera
 filename = 'weather_model.sav'
 model = pickle.load(open(filename, 'rb'))
 tables = pd.read_html("http://hydro.imd.gov.in/hydrometweb/(S(ht2dew45izstmbyyphslh455))/landing.aspx#")
@@ -17,3 +17,28 @@ states={'ANDAMAN & NICOBAR ISLANDS': 0, 'ARUNACHAL PRADESH': 1, 'ASSAM':2, 'MEGH
 'LAKSHADWEEP': 18, 'MADHYA MAHARASHTRA': 19, 'MATATHWADA': 20, 'NAGALAND':21, 'MANIPUR':21, 'MIZORAM':21, 'TRIPURA': 21, 'KARNATAKA': 22,
 'ODISHA': 23, 'PUNJAB': 24, 'RAYALSEEMA': 25, 'SAURASHTRA & KUTCH': 26, 'SOUTH INTERIOR KARNATAKA': 27, 'SUB HIMALAYAN WEST BENGAL & SIKKIM': 28,
 'TAMIL NADU': 29, 'TELANGANA': 30, 'UTTARAKHAND': 31, 'VIDARBHA': 32, 'WEST MADHYA PRADESH': 33, 'WEST RAJASTHAN': 34, 'WEST UTTAR PRADESH': 35}
+
+#  read t
+tables=np.array(tables[4])
+# print(tables)
+tables=np.array(tables[1:])
+
+@app.route('/predict_calamities', methods=['POST'])
+def predict_calamities():
+    #json= {'district': value, 'state': 'ODISHA')
+    data= request.get_json(force=True)
+    a = max(tables[tables[:, 1] == (data['district'].upper())][:, 2].flatten(), default=-1)
+    if(a== -1):
+        return jsonify('Invalid District')
+    else:
+
+        prediction= model.predict([[states['ODISHA'],a]])
+
+        if (prediction == 0):
+            return jsonify("Scare Rainfall, Chances of Drought")
+        elif (prediction == 1):
+            return jsonify("Moderate rain falling")
+        elif (prediction == 2):
+            return jsonify("Heavy raining, chances of floods.")
+        else:
+            return jsonify("Severe Flood chances are at peak.")
